7ddfe346a08b4b497fe567339ebe399f7500f938
Cheolgu Kim <cgkim@nowcom.com>
2022-07-04 23:50:16 -0700

    https://nextjs.org/learn/basics/dynamic-routes/polishing-index-page

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,8 +1,28 @@
-import Layout from '../../components/layout';
-import { getAllPostIds } from '../../lib/posts';
+import Head from 'next/head';
 
-export default function Post() {
-  return <Layout>...</Layout>;
+import utilStyles from '../../styles/utils.module.css';
+
+import Layout from '../../components/layout';
+import Date from '../../components/date';
+
+import { getAllPostIds, getPostData } from '../../lib/posts';
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      {/* Add this <Head> tag */}
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
 }
 
 export async function getStaticPaths() {
@@ -10,5 +30,14 @@ export async function getStaticPaths() {
   return {
     paths,
     fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
   };
 }
